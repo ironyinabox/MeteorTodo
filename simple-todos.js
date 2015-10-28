d5371bfcb432a69da02234cc5bf74e5f8af9dd63
ironyinabox <ironyinabox@gmail.com>
2015-10-28 13:09:52 -0400

    add Tasks collection, task submit form

--- simple-todos.js
+++ simple-todos.js
@@ -1,13 +1,29 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
 
-
   Template.body.helpers({
-    tasks: [
-      {text: "My first task"},
-      {text: "My second task"},
-      {text: "My third task"}
-    ]
+    tasks: function () {
+      return Tasks.find({});
+    }
   });
+
+  Template.body.events({
+    "submit .new-task": function (e) {
+      e.preventDefault();
+
+      //event.target.inputName.value
+      var text = e.target.text.value
+
+      //property: value
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      //Clear form
+      e.target.text.value = "";
+    }
+  })
 }
 
 if (Meteor.isServer) {
